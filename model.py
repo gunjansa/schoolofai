59db6b52c15d3ab54e57ccae1fd07ec0f03777eb
gsarda <gunjan.mait@gmail.com>
2024-01-09 05:21:51 +0530

    Changes to modularize the Model Training Code

--- model.py
+++ model.py
@@ -1,1 +1,121 @@
 # File to create Model Definition
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from torchsummary import summary
+
+class Net(nn.Module):
+    #This defines the structure of the NN.
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.fc1 = nn.Linear(4096, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x), 2)
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(self.conv3(x), 2)
+        x = F.relu(F.max_pool2d(self.conv4(x), 2))
+        # x = F.relu(F.max_pool2d(x, 2))
+        # x = F.relu(F.max_pool2d(x, 2))
+        x = x.view(len(x), 4096)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+# Layer structure or architecture
+# 28x28x1
+# 26x26x32
+# 24x24x64
+# 12x12x64
+# 10x10x128
+# 8x8x256
+# 4x4x256
+# 320x50
+# 50x10
+    
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+# Data to plot accuracy and loss graphs
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+def train(model, device, train_loader, optimizer, criterion):
+    model.train()
+    pbar = tqdm(train_loader)
+
+    train_loss = 0
+    correct = 0
+    processed = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # Predict
+        pred = model(data)
+
+        # Calculate loss
+        loss = criterion(pred, target)
+        train_loss+=loss.item()
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        correct += GetCorrectPredCount(pred, target)
+        processed += len(data)
+
+        pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+    train_acc.append(100*correct/processed)
+    train_losses.append(train_loss/len(train_loader))
+
+def test(model, device, test_loader, criterion):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            # test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += criterion(output, target).item()  # sum up batch loss
+
+            correct += GetCorrectPredCount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+def draw_graphs():
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
+
+def get_model_summary(model, device):
+    return summary(model, input_size=(1, 28, 28))
---
+++ utils.py
@@ -0,0 +1,53 @@
+import torch
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+def create_mnist_dataloader():
+    # Train data transformations
+    # We train on complicated data by applying transformations to introduce Noise
+    train_transforms = transforms.Compose([
+        # This is applied to introduce some noise (like, Masking).
+        # Example, if training is done to predict  Nurse from hospital teachers.
+        # Then, model could be biased that by looking females it just predicts Nurse.
+        transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
+        transforms.Resize((28, 28)),
+        # There are chances that images are rotated. Like, if 7 is rotated it might predict as 1
+        transforms.RandomRotation((-15., 15.), fill=0),
+        transforms.ToTensor(),
+        # In general we do normalize all the images with Mean and Standard Deviation. Why? Need to see previous lecture
+        transforms.Normalize((0.1307,), (0.3081,)),
+        ])
+
+    # Test data transformations
+    # In test data, we don't apply transformations
+    test_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1407,), (0.4081,))
+        ])
+
+    train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+    # Changed
+    # test_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+    test_data = datasets.MNIST('../data', train=False, download=True, transform=test_transforms)
+
+    batch_size = 512
+
+    kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}
+
+    # test_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+    test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
+    train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+    return train_loader, test_loader
+
+def get_dataset_images(train_loader, total_images=12):
+    batch_data, batch_label = next(iter(train_loader))
+
+    fig = plt.figure()
+
+    for i in range(total_images):
+        plt.subplot(3,4,i+1)
+        plt.tight_layout()
+        plt.imshow(batch_data[i].squeeze(0), cmap='gray')
+        plt.title(batch_label[i].item())
+        plt.xticks([])
+        plt.yticks([])
