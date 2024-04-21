66acbf5ea646c46906f82277882a5bd77326e04f
gunjansa <44889840+gunjansa@users.noreply.github.com>
2024-04-21 11:57:45 +0530

    Added one more Neural Network

--- model_assignment_6.py
+++ model_assignment_6.py
@@ -22,3 +22,26 @@ class Net(nn.Module):
         x = F.relu(self.conv7(x))
         x = x.view(-1, 10)
         return F.log_softmax(x)
+
+class Net2(nn.Module):
+    #This defines the structure of the NN.
+    def __init__(self):
+        super(Net3, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(4096, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv2(F.relu(self.conv1(x)))))
+        x = self.pool2(F.relu(self.conv4(F.relu(self.conv3(x)))))
+        # x = F.relu(F.max_pool2d(x, 2))
+        # x = F.relu(F.max_pool2d(x, 2))
+        x = x.view(len(x), 4096)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
